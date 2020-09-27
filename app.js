a55dd9c699e875b47a6e2d798ab826cb2be00186
francis-poon <francool97@gmail.com>
2020-09-27 04:40:00 -0400

    added description to MongoDB

--- app.js
+++ app.js
@@ -41,21 +41,23 @@ App.get('/', (req, res) => {
 
 App.post('/submitquiz', async (req, res) => {
 	uniqueCode = ResultCode();
-	sandwichResult = req.body.sandwwichResult;
+	result = req.body.result;
+	resultDescription = req.body.resultDescription;
 	
 	const datapoint = new DataDB({
-		code: code,
-		result:result
+		code: uniqueCode,
+		result: sandwichResult,
+		description: resultDescription
 	});
 	
 	await datapoint.save()
 	
-	res.redirect('/result/' + code)
+	res.redirect('/result/' + uniqueCode)
 });
 
 App.get('/result/:code', async (req, res) => {
 	const query = await DataDB.find({code: req.params.code})
-	res.render('result', {userCode: code, userResult: result});
+	res.render('result', {userCode: code, userResult: query.result, userDescription: query.description});
 });
 
 App.listen(PORT, () => {
